9cdd0ab718035077787708a75b230e16f707a9cf
Sascha Rose <sascha.rose@gmail.com>
2024-10-30 21:02:34 +0100

    add simple sample

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { QrReader } from "@cmdnio/react-qr-reader";
+import QRCode from "react-qr-code";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [data, setData] = useState("No result");
 
   async function fetchUsers() {
     const response = await fetch("http://localhost:3000/users");
@@ -18,25 +21,54 @@ function App() {
     fetchUsers();
   }, []);
 
-  async function handleUserOnClick() {
-    const response = await fetch("http://localhost:3000/users", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        name: "John Doe",
-        age: 29,
-      }),
-    });
-    if (!response.ok) {
-      console.warn("Response is not OK!");
-    }
-    fetchUsers();
-    console.debug("Good!");
-  }
+  // async function handleUserOnClick() {
+  //   const response = await fetch("http://localhost:3000/users", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       name: "John Doe",
+  //       age: 29,
+  //     }),
+  //   });
+  //   if (!response.ok) {
+  //     console.warn("Response is not OK!");
+  //   }
+  //   fetchUsers();
+  //   console.debug("Good!");
+  // }
 
   return (
     <div>
       <div>
+        <QRCode
+          size={256}
+          style={{
+            height: "auto",
+            maxWidth: "100%",
+            width: "100%",
+            marginBottom: "15rem",
+          }}
+          value={"ich wünsche euch einen schönen Tag"}
+          viewBox={`0 0 256 256`}
+        />
+        <QrReader
+          onResult={(result, error) => {
+            if (result) {
+              setData(result?.text);
+            }
+
+            if (error) {
+              console.info(error);
+            }
+          }}
+          containerStyle={{ width: 300, height: 400 }}
+          constraints={{
+            facingMode: "environment",
+          }}
+        />
+        <pre>{data}</pre>
+      </div>
+      <div style={{ marginBottom: "15rem" }}>
         {users.map(({ _id, name, age }) => (
           <div key={_id}>
             <p>
@@ -45,8 +77,7 @@ function App() {
           </div>
         ))}
       </div>
-
-      <button onClick={handleUserOnClick}>add user</button>
+      {/* <button onClick={handleUserOnClick}>add user</button> */}
     </div>
   );
 }
